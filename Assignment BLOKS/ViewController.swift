1a2efd94e00920ef35aa36eeb2eb622c05474a4b
Ivan <Ivan@macbook-pro-umanets.local>
2019-02-06 18:50:10 +0200

    Initial Commit

---
+++ Assignment BLOKS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment BLOKS
+//
+//  Created by Ivan on 06/02/2019.
+//  Copyright © 2019 Ivan Umanets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
